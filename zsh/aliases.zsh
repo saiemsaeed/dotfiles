d579c4a9574c65a12ac7a1b1127676b2ce53ac71
Saiem Saeed <saiem.saeed7@gmail.com>
2026-03-11 09:18:28 +0100

    added nic command

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -27,6 +27,13 @@ function oc() {
     opencode
 }
 
+
+# AO function - updates ZTOKEN from ztoken script and runs opencode
+function dr() {
+    export ZTOKEN=$(ztoken)
+    droid
+}
+
 function lg() {
     export LAZYGIT_NEW_DIR_FILE=~/.lazygit/newdir
 
@@ -40,4 +47,36 @@ function lg() {
 
 # Script runner
 alias ss="run_scripts"
+
+alias cpr="gh pr create --web"
+
+# NIC - splits current Ghostty window into: vim (left) | opencode (top-right) / empty (bottom-right)
+function nic() {
+    local dir="${1:-$PWD}"
+    local ztoken="$(ztoken)"
+    osascript - "$dir" "$ztoken" <<'APPLESCRIPT'
+on run argv
+    set workDir to item 1 of argv
+    set ztokenVal to item 2 of argv
+    tell application "Ghostty"
+        set currentTab to selected tab of window 1
+        set leftPane to terminal 1 of currentTab
+
+        set cfg to new surface configuration
+        set initial working directory of cfg to workDir
+
+        set cfgOC to new surface configuration from cfg
+        set command of cfgOC to "zsh -l -c 'ZTOKEN=" & ztokenVal & " opencode'"
+
+        set rightPane to split leftPane direction right with configuration cfgOC
+        split rightPane direction down with configuration cfg
+
+        input text "nvim" to leftPane
+        send key "enter" to leftPane
+
+        focus rightPane
+    end tell
+end run
+APPLESCRIPT
+}
 
